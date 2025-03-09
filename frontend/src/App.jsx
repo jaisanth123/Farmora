e9026adab060da8263e48b5d4300798dce2d7881
Rashmika K R <krrashmika2004@gmail.com>
2025-03-09 13:09:33 +0530

    fastapi application init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/firebase/Login";
 import Register from "./components/firebase/Register";
-
+//import FarmerRegistrationForm from "./components/FarmerRegistrationForm";
 function App() {
   return (
     <Router>
@@ -9,6 +9,7 @@ function App() {
         <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        {/* <Route path="/form" element={<FarmerRegistrationForm />} /> */}
       </Routes>
     </Router>
   );
